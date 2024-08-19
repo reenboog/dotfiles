7b624c973fa69ef63cc7439698f715bf8f42dc3b
Alex Gievsky <alex.gievsky@gmail.com>
2024-08-20 01:01:05 +0200

    use cp instead of symlinking.

--- setup.sh
+++ setup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-ln -sf ~/dotfiles/.vimrc ~/.vimrc
-ln -sf ~/dotfiles/.zshrc ~/.zshrc
+
+# Copy dotfiles to home directory
+cp ~/dotfiles/.vimrc ~/.vimrc
+cp ~/dotfiles/.zshrc ~/.zshrc
+cp ~/dotfiles/.aliases ~/.aliases
+
+# Install vim-plug
 mkdir -p ~/.vim/plugged
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Install Vim plugins
 vim +PlugInstall +qall
+
+# Remove the dotfiles repository folder
+rm -rf ~/dotfiles
